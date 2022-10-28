12df6aeb976c4ec9f2df4114a0b45bfa83f17f28
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-10-28 13:05:13 +0400

    maven

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,9 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.*;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return !org.apache.commons.lang3.StringUtils.startsWith(str, "-");
     }
 }

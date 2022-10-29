1ee17977770a28fe100b994e231e7126613012bf
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-10-29 14:56:03 +0400

    ...

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,9 +1,9 @@
 package com.epam.utils;
 
-import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.math.*;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return NumberUtils.isParsable(str) && NumberUtils.toFloat(str) > 0;
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }

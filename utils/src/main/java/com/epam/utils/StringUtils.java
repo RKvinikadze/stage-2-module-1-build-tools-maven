722f852dfe1be4c988fae6d99415294848116f65
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-10-29 14:46:03 +0400

    ispositivenumber

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,9 +1,9 @@
 package com.epam.utils;
 
-import org.apache.commons.lang3.*;
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return !org.apache.commons.lang3.StringUtils.startsWith(str, "-");
+        return NumberUtils.isParsable(str) && NumberUtils.toFloat(str) > 0;
     }
 }
